2ee7f1c49b2cf19b4998d9a0df49854001609e4e
mathias friis <mathiasfriisiversen@gmail.com>
2025-09-15 13:34:56 +0200

    added scaffolding, might need to fix entities placement within the scaffolding command inside scaffold.ps1

---
+++ server/scaffold/scaffold.ps1
@@ -0,0 +1,12 @@
+﻿# Read and parse .env file
+Get-Content .env | ForEach-Object {
+if ($_ -match '^([^#][^=]+)=(.*)$') {
+$name = $matches[1].Trim()
+$value = $matches[2].Trim()
+[Environment]::SetEnvironmentVariable($name, $value, "Process")
+}
+}
+
+# Now you can use the connection string
+dotnet tool install -g dotnet-ef
+dotnet ef dbcontext scaffold $env:CONN_STR Npgsql.EntityFrameworkCore.PostgreSQL --context MyDbContext --no-onconfiguring --schema library --force
