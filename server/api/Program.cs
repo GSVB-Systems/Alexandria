5080ed6a51ee89c71c9f862886eb92b23fe7096b
Benjamin Davgaard <57093018+benj4515@users.noreply.github.com>
2025-09-15 18:38:39 +0200

    api statup with SSL Handshake error

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,6 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using scaffold;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<MyDbContext>(conf =>
+{
+ conf.UseNpgsql("Host=ep-calm-moon-agsgalrn-pooler.c-2.eu-central-1.aws.neon.tech; Database=neondb; Username=neondb_owner; Password=npg_FbPxLGuKqJ29; SSL Mode=VerifyFull; Channel Binding=Require;");
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", ([FromServices] MyDbContext dbContext) =>
+{  
+    
+    
+    
+    var myAuthor = new Author
+    {
+        Id = Guid.NewGuid().ToString(),
+        Name = "John Doe",
+        Createdat = DateTime.UtcNow
+    };
+    dbContext.Authors.Add(myAuthor);
+    dbContext.SaveChanges();
+    
+  var authors = dbContext.Authors.ToList();
+    var books = dbContext.Books.ToList();
+    var genres = dbContext.Genres.ToList();
+});
 
 app.Run();
