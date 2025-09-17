f7a46b2940a8f586182c535ff2c9401fe25703ca
mathias friis <mathiasfriisiversen@gmail.com>
2025-09-17 11:40:39 +0200

    DTO for Genre + Book + Request DTOS for Genre
    DTOgate :(

---
+++ server/api/DTOs/BookDTO.cs
@@ -0,0 +1,25 @@
+﻿
+using scaffold;
+
+namespace api.DTOs;
+
+public class BookDTO
+{
+    public BookDTO(Book bookEntity)
+    {
+        Id = bookEntity.Id;
+        Title = bookEntity.Title;
+        Pages = bookEntity.Pages;
+        Createdat = bookEntity.Createdat;
+        if (bookEntity.Genre != null) Genre = new GenreDTO(bookEntity.Genre);
+        AuthorsIds = bookEntity.Authors?.Select(a => a.Id).ToList() ?? new List<string>();
+    }
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public int Pages { get; set; }
+    public DateTime? Createdat { get; set; }
+    
+    public virtual GenreDTO? Genre { get; set; }
+    
+    public virtual ICollection<string> AuthorsIds { get; set; }
+}
---
+++ server/api/DTOs/Requests/CreateGenreDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.DTOs.Requests;
+
+public record CreateGenreDTO
+{
+    [Required] [MinLength(1)] public string newGenreId { get; set; }
+    
+    [Required] [MinLength(1)] public string NewGenreName { get; set; }
+}
